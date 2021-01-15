f83ad94a99e606abd0834d3c48faf80c3ce97e89
Tejveer Singh Bath <tejveer.bath@tatadigital.com>
2021-01-15 15:12:49 +0530

    MonotonicArray Completed

--- MonotonicArray.js
+++ MonotonicArray.js
@@ -1,10 +1,10 @@
 const array = [-1, -5, -10, -1100,-1100,-1101, -1102, -9001]
 if (array.length === 1) console.log(true)
-let flag = 0
+let incre = 0
+let decre = 0
 for (let index = 0; index < array.length-1; index++) {
-    if ((array[index] >= array[index+1])) continue
-    else if ((array[index] <= array[index+1])) continue
-    else flag++
+    if ((array[index] >= array[index+1])) incre++
+    else if ((array[index] <= array[index+1])) decre++
 }
-if (flag === 0) console.log(true)
+if ((incre > 0 && decre === 0) || (incre === 0 && decre > 0)) console.log(true)
 else console.log(false)
