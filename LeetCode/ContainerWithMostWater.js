cdd7a02c13b9fcd741195f4511235687e973fcd1
Tejveer Singh Bath <tejveer.bath@tatadigital.com>
2021-01-19 23:04:00 +0530

    First Leet Code Done and New folder created

---
+++ LeetCode/ContainerWithMostWater.js
@@ -0,0 +1,18 @@
+const height = [4,3,2,1,4]
+let maxArea = 1
+for (let i = 0; i < height.length-1; i++) {
+    for (let j = i+1; j < height.length; j++) {
+        if (height[i] >= height[j]) {
+            let area = height[j]*(j-i)
+            if (area > maxArea) {
+                maxArea = area
+            }
+        }
+        else{
+            let area = height[i]*(j-i)
+            if (area > maxArea) {
+                maxArea = area
+            }
+        }
+    }
+}
